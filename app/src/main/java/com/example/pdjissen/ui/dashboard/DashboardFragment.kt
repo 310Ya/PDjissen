dae6f9b754421bbaac192744ca211ef18d1e0f8e
c2066 <c2066383@gmail.com>
2025-10-03 15:28:49 +0900

    s

--- app/src/main/java/com/example/pdjissen/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/pdjissen/ui/dashboard/DashboardFragment.kt
@@ -14,7 +14,7 @@ class DashboardFragment : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
-    // onDestroyView.
+    // on
     private val binding get() = _binding!!
 
     override fun onCreateView(
