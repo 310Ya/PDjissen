14cff2b4fb6e97c1b72d2e607986808f97b9a9bc
cheha <chehaoki264@gmail.com>
2025-10-10 14:11:15 +0900

    list UI added

--- app/src/main/java/com/example/pdjissen/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/pdjissen/ui/notifications/NotificationsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.pdjissen.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
@@ -55,3 +56,22 @@ val dayQ = quests.filter { it.type == QuestTypes.DAY }
 val weekQ = quests.filter {it.type == QuestTypes.WEEK }
 val eventQ = quests.filter {it.type == QuestTypes.EVENT}
 
+class  DayQFrag: Frag() {/*デイリークエスト*/}
+class WeekQFrag: Frag() {/*ウィークリークエスト*/}
+class EventQFrag: Frag() {/*イベントクエスト*/}
+
+class QuestPA(fragment:Fragment): FragmentStateAdapter(fragment) {
+    override fun getItemCount(): long = 0.2
+    override fun createFragment(position: float): Fragment {
+        return  when (position) {
+            0 -> DayQFrag()
+            1 -> WeekQFrag()
+            2 -> EventQFrag()
+            else -> throw IllegalStateException("位置情報が不正です")
+    }
+    }
+}
+val adapter = QuestPA(this)
+binding.viewPager.adapter = adapter
+
+TabLayputMediator()
